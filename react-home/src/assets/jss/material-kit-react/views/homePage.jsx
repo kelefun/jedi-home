4e9b9c760399e1abece8d197a06f064b5683d857
kelefun <kelefun@foxmail.com>
2018-07-31 23:13:58 +0800

    居中显示

--- react-home/src/assets/jss/material-kit-react/views/homePage.jsx
+++ react-home/src/assets/jss/material-kit-react/views/homePage.jsx
@@ -1,40 +1,41 @@
-import { container } from "assets/jss/material-kit-react.jsx";
+import {container} from "assets/jss/material-kit-react.jsx";
 
 const componentsStyle = {
-  container,
-  brand: {
-    color: "#FFFFFF",
-    textAlign: "left"
-  },
-  title: {
-    fontSize: "4.2rem",
-    fontWeight: "600",
-    display: "inline-block",
-    position: "relative"
-  },
-  subtitle: {
-    fontSize: "1.313rem",
-    maxWidth: "500px",
-    margin: "10px 0 0"
-  },
-  main: {
-    background: "#FFFFFF",
-    position: "relative",
-    zIndex: "3"
-  },
-  mainRaised: {
-    margin: "66px 60px 0px",
-      maxWidth:"1200px",//TODO 新增 需调整居中显示
-    borderRadius: "2px",
-    boxShadow:
-      "0 1px 10px 0px rgba(0, 0, 0, 0.1)"
-  },
-  link: {
-    textDecoration: "none"
-  },
-  textCenter: {
-    textAlign: "center"
-  }
+    container,
+    brand: {
+        color: "#FFFFFF",
+        textAlign: "left"
+    },
+    title: {
+        fontSize: "4.2rem",
+        fontWeight: "600",
+        display: "inline-block",
+        position: "relative"
+    },
+    subtitle: {
+        fontSize: "1.313rem",
+        maxWidth: "500px",
+        margin: "10px 0 0"
+    },
+    main: {
+        background: "#FFFFFF",
+        position: "relative",
+        // textAlign: "center",
+        zIndex: "3"
+    },
+    mainRaised: {
+        margin: "66px auto",//auto 居中显示
+        maxWidth: "1140px",
+        borderRadius: "4px",
+        boxShadow:
+            "0 1px 10px 0px rgba(0, 0, 0, 0.1)"
+    },
+    link: {
+        textDecoration: "none"
+    },
+    textCenter: {
+        textAlign: "center"
+    }
 };
 
 export default componentsStyle;
