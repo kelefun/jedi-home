564573eb5de031a86dcc6089728161c6788b0234
liukaiyang <liukaiyang@souche.com>
2018-09-29 21:01:26 +0800

    添加heartbeat

--- home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
+++ home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.jedijava.home.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -21,7 +22,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/topic", "/queue")
+                .setTaskScheduler(new DefaultManagedTaskScheduler())
+                .setHeartbeatValue(new long[]{10000,10000});
     }
 }
