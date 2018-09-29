4eeddf5895ed8fdd8772fcfe2d72aafd59b0f370
liukaiyang <liukaiyang@souche.com>
2018-09-29 17:55:04 +0800

    websocket 依赖

---
+++ home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.jedijava.home.web.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * 
+ * @author liukaiyang
+ * @date 2018/9/29 15:31
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/export").setAllowedOrigins("*").withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic", "/queue");
+    }
+}
