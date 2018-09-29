8cc70af43d374e492cbff63e49056080769e9c58
liukaiyang <liukaiyang@souche.com>
2018-09-29 20:57:08 +0800

    fix err ->原生连接不能用withsocketjs

--- home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
+++ home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/export").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/export").setAllowedOrigins("*");
     }
 
     @Override
