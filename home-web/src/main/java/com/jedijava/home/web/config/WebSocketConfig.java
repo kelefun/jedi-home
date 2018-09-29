d933900957ff359dd40eaaf073abb984c60cd91a
liukaiyang <liukaiyang@souche.com>
2018-09-29 20:52:59 +0800

    不重要更新

--- home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
+++ home-web/src/main/java/com/jedijava/home/web/config/WebSocketConfig.java
@@ -7,18 +7,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
- * 
  * @author liukaiyang
  * @date 2018/9/29 15:31
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/export").setAllowedOrigins("*").withSockJS();
     }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
